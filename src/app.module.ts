0bff5f5c0d474e7e7d09ff6cd227fec333757cc1
gabrieladipoggio <gabrieladipoggio@gmail.com>
2022-06-06 14:23:50 -0300

    Adiciona configurações do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { databaseProviders } from './core/database/database.providers';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+  ],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  providers: [AppService, UserService, ...databaseProviders],
+  exports: [...databaseProviders],
 })
 export class AppModule {}
